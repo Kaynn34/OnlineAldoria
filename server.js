25ad286064b958bce770645574ac621bc3f82fdb
Kaynn34 <155450328+Kaynn34@users.noreply.github.com>
2026-05-29 03:02:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const root = __dirname;
 const port = Number(process.env.PORT || 4173);
@@ -11,6 +12,7 @@ const types = {
   ".js": "text/javascript; charset=utf-8",
   ".png": "image/png",
   ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
   ".webp": "image/webp",
   ".svg": "image/svg+xml; charset=utf-8"
 };
@@ -37,6 +39,49 @@ const server = http.createServer((request, response) => {
       "Content-Type": types[path.extname(filePath)] || "application/octet-stream"
     });
     response.end(data);
+  });
+});
+
+const io = new Server(server);
+const players = {};
+
+io.on("connection", (socket) => {
+  socket.on("player:join", (player) => {
+    players[socket.id] = {
+      id: socket.id,
+      name: player.name || "Joueur",
+      area: player.area || "outside",
+      mapKey: player.mapKey || "outside",
+      x: player.x || 0,
+      y: player.y || 0,
+      skin: player.skin || 0,
+      classKey: player.classKey || null
+    };
+
+    socket.emit("players:init", players);
+    socket.broadcast.emit("player:joined", players[socket.id]);
+  });
+
+  socket.on("player:update", (player) => {
+    if (!players[socket.id]) return;
+
+    players[socket.id] = {
+      ...players[socket.id],
+      name: player.name || players[socket.id].name,
+      area: player.area || players[socket.id].area,
+      mapKey: player.mapKey || players[socket.id].mapKey,
+      x: player.x,
+      y: player.y,
+      skin: player.skin || 0,
+      classKey: player.classKey || null
+    };
+
+    socket.broadcast.emit("player:updated", players[socket.id]);
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    socket.broadcast.emit("player:left", socket.id);
   });
 });
 
